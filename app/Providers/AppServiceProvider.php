2d7c1edcb56f96037bed466baebb99ac443dd4b7
Farid Moreno <farid.leonardo98@gmail.com>
2024-06-17 22:38:49 -0500

    feat(providers): set datetime global format

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,18 @@
 
 namespace App\Providers;
 
+use Filament\Tables\Table;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        Table::$defaultDateTimeDisplayFormat = 'M j, Y g:i a';
     }
 }
