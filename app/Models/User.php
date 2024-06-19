9716a5172ff0b48639d21c613f8fd6347113bcac
Farid Moreno <farid.leonardo98@gmail.com>
2024-06-18 23:33:03 -0500

    feat: import traits, add relationships and fields on user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,17 +2,28 @@
 
 namespace App\Models;
 
+use App\Enums\UserStatus;
+use App\Traits\Models\HasCreatedBy;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Models\Contracts\HasName;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser, HasName
 {
-    use HasFactory, HasRoles, Notifiable;
+    use HasCreatedBy, HasFactory, HasRoles, Notifiable;
 
     protected $fillable = [
-        'name',
+        'identification_number',
+        'full_name',
+        'birth_date',
+        'phone_number',
+        'address',
+        'status',
         'email',
         'password',
     ];
@@ -22,9 +33,26 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->status === UserStatus::ACTIVE;
+    }
+
+    public function getFilamentName(): string
+    {
+        return "{$this->full_name}";
+    }
+
+    // Relationships
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
     protected function casts(): array
     {
         return [
+            'status' => UserStatus::class,
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
