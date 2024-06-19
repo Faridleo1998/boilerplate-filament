b5125ac63d364bb221c6089408296d2098e58809
Farid Moreno <farid.leonardo98@gmail.com>
2024-06-19 18:05:10 -0500

    feat: enable profile and disable breadcrumbs in admin panel provider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,8 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->profile()
+            ->breadcrumbs(false)
             ->colors([
                 'primary' => Color::Amber,
             ])
