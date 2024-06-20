dbc1a57f2396e24ea9308e00c48dfad8a47d3e33
Farid Moreno <farid.leonardo98@gmail.com>
2024-06-20 18:01:52 -0500

    chore(i18n): add translations

---
+++ lang/es/labels.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'address' => 'Dirección',
+    'birth_date' => 'Fecha de cumpleaños',
+    'created_at' => 'Fecha de creación',
+    'created_by' => 'Creado por',
+    'email' => 'Correo electrónico',
+    'full_name' => 'Nombre',
+    'identification_number' => 'Identificación',
+    'name' => 'Nombre',
+    'password' => 'Contraseña',
+    'phone_number' => 'Teléfono',
+    'status' => 'Estado',
+    'updated_at' => 'Fecha de actualización',
+];
--- lang/es/resources.php
+++ lang/es/resources.php
@@ -3,5 +3,13 @@
 return [
     'user' => [
         'singular_label' => 'Usuario',
+        'labels' => [
+            'roles_count' => 'Roles',
+        ],
+        'sections' => [
+            'personal_information' => 'Información personal',
+            'contact_information' => 'Información de contacto',
+            'account_information' => 'Información de la cuenta',
+        ],
     ],
 ];
