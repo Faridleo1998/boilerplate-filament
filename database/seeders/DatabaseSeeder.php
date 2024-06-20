e68d164431258752ce20a1290dfae0cb470f8f7c
Farid Moreno <farid.leonardo98@gmail.com>
2024-06-20 16:56:42 -0500

    feat: add nnjeim/world seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            WorldSeeder::class,
             UserSeeder::class,
         ]);
     }
