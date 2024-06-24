ef1d8aad0c67f0bc341e10f4b91f82caefdef596
Farid Moreno <farid.leonardo98@gmail.com>
2024-06-24 17:55:16 -0500

    chore(i18n): add social networks labels

---
+++ lang/es/enums/social-network.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'whatsapp' => 'WhatsApp',
+    'facebook' => 'Facebook',
+    'instagram' => 'Instagram',
+    'x_twitter' => 'Twitter',
+    'youtube' => 'YouTube',
+    'linkedin' => 'LinkedIn',
+    'tiktok' => 'TikTok',
+    'pinterest' => 'Pinterest',
+];
