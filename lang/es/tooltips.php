564aac7ab241730e6d82cce5e7377e03d852b89f
Farid Moreno <farid.leonardo98@gmail.com>
2024-07-03 10:01:04 -0500

    chore(i18n): add tooltip translation

--- lang/es/tooltips.php
+++ lang/es/tooltips.php
@@ -2,4 +2,7 @@
 
 return [
     'is_featured' => 'Muestra el cliente en los primeros lugares de los resultados de búsqueda.',
+    'user' => [
+        'status' => 'Activa o desactiva la cuenta del usuario. Los usuarios desactivados no podrán acceder al sistema.',
+    ],
 ];
