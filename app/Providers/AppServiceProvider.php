a95d4f09fb3ec2a3a50c5ce848237164796dcb47
Farid Moreno <farid.leonardo98@gmail.com>
2024-07-08 16:05:41 -0500

    feat: show user name in top bar - service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use Filament\Forms\Components\Toggle;
+use Filament\Support\Facades\FilamentView;
 use Filament\Tables\Table;
+use Filament\View\PanelsRenderHook;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +22,11 @@ class AppServiceProvider extends ServiceProvider
         Toggle::configureUsing(function (Toggle $toggle): void {
             $toggle->inline(false);
         });
+
+        FilamentView::registerRenderHook(
+            PanelsRenderHook::USER_MENU_BEFORE,
+            fn() => auth()->user()->full_name,
+        );
 
         if ($this->app->environment('local')) {
             $this->app->register(TelescopeServiceProvider::class);
