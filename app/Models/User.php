6c3addda8ac3ccea3f0ce0c98fdaca423f5577cf
Farid Moreno <farid.leonardo98@gmail.com>
2024-07-08 22:12:09 -0500

    feat: add customers relation in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Filament\Models\Contracts\HasName;
 use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
@@ -49,6 +50,12 @@ class User extends Authenticatable implements FilamentUser, HasName
         return $this->belongsTo(User::class, 'created_by');
     }
 
+    public function customers(): HasMany
+    {
+        return $this->hasMany(Customer::class, 'created_by');
+    }
+
+    // Casts
     protected function casts(): array
     {
         return [
