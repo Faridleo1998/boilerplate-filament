a3bd56162e53b460abecc887c0c785e2901519b9
Farid Moreno <farid.leonardo98@gmail.com>
2024-07-12 16:58:56 -0500

    chore(i18n): add export_completed notification - es

--- lang/es/notifications.php
+++ lang/es/notifications.php
@@ -2,4 +2,5 @@
 
 return [
     'error_occurred' => 'Ha ocurrido un error. Por favor, inténtalo de nuevo.',
+    'export_completed' => 'La exportación de :resource se ha completado. :rowsCount registros exportados.',
 ];
