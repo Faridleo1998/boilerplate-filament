fa18bd3e85857b1313d8caf212d5bf7cf3dc42a8
Farid Moreno <farid.leonardo98@gmail.com>
2024-07-15 15:06:08 -0500

    feat: optimize paginate customers

--- app/Filament/Resources/CustomerResource/Pages/ListCustomers.php
+++ app/Filament/Resources/CustomerResource/Pages/ListCustomers.php
@@ -7,17 +7,25 @@ use App\Filament\Resources\CustomerResource;
 use App\Models\Customer;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Gate;
 
 class ListCustomers extends ListRecords
 {
     protected static string $resource = CustomerResource::class;
 
+    protected function paginateTableQuery(Builder $query): Paginator
+    {
+        return $query->fastPaginate(($this->getTableRecordsPerPage() === 'all') ? $query->count() : $this->getTableRecordsPerPage());
+    }
+
     protected function getHeaderActions(): array
     {
         return [
             Actions\ExportAction::make()
                 ->label(__('actions.export'))
+                ->modalHeading(__('actions.export') . ' ' . __('resources.customer.plural_label'))
                 ->color('success')
                 ->icon('heroicon-o-arrow-down-tray')
                 ->exporter(CustomerExporter::class)
