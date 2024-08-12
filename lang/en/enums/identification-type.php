9d868a5f3b4a3dc1f88159385b447d08f1d3578e
Farid Moreno <farid.leonardo98@gmail.com>
2024-08-12 18:11:00 -0500

    chore(i18n): add tooltips and identification type translations - en

---
+++ lang/en/enums/identification-type.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'cc' => 'CC (Citizenship Card)',
+    'nit' => 'NIT (Tax Identification Number)',
+    'pas' => 'PAS (Passport)',
+    'ce' => 'CE (Foreigners Identification Card)',
+];
---
+++ lang/en/tooltips.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'is_featured' => 'Show the client at the top of search results.',
+    'user' => [
+        'status' => 'Activate or deactivate the user account. Deactivated users will not be able to access the system.',
+    ],
+    'setting' => [
+        'default_country' => 'Default country for record creation.',
+        'default_state' => 'Default state for record creation.',
+        'default_city' => 'Default city for record creation.',
+        'use_default_location' => 'Use location as the default for record creation.',
+        'theme_color' => 'Accent color for the user interface. Leave blank to use the default color.',
+    ],
+];
