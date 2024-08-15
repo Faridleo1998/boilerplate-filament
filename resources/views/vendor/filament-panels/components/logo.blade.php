241ab340c7e9b4b2cd14d379f6b5cacc7a0b53c7
Farid Moreno <farid.leonardo98@gmail.com>
2024-08-15 18:16:57 -0500

    feat: change styles logo view

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -3,9 +3,9 @@
 @endphp
 
 @if (file_exists(public_path('storage/images/logo.webp')))
-    <img src="{{ $logoPath }}" class="h-16" alt="Logo">
+    <img src="{{ $logoPath }}" style="max-height: 50px !important; max-width: 200px !important;" alt="Logo">
 @else
-    <span class="font-bold text-lg">
+    <span class="font-bold text-2xl">
         {{ \Illuminate\Support\Str::limit(config('app.name'), 25, '...') }}
     </span>
 @endif
